74b0d9366ee08a1879ba11f3f0d60a0c82a19ad8
matmaxalez <mateusz@kapica.be>
2026-08-22 16:14:07 +0200

    build: add Vite configuration

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    target: 'es2022',
+    sourcemap: true,
+    reportCompressedSize: true,
+  },
+  server: {
+    host: true,
+  },
+});
